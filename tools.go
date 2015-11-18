3a6ed954663c0606ff14740e3cddd37a5d03538e
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2015-11-18 09:42:13 +0100

    Add GetHostName() call

--- tools.go
+++ tools.go
@@ -1,7 +1,9 @@
 package cfutil
 
 import (
+	"errors"
 	"os"
+	"strings"
 )
 
 func IsFirstInstance() bool {
@@ -11,6 +13,19 @@ func IsFirstInstance() bool {
 		return true
 	}
 	return false
+}
+
+func GetHostname() (string, error) {
+	appEnv, err := Current()
+	if err != nil {
+		return "", err
+	}
+	if len(appEnv.ApplicationURIs) == 0 {
+		return "", errors.New("Unknown hostname")
+	}
+	hostWithPort := appEnv.ApplicationURIs[0]
+	parts := strings.Split(hostWithPort, ":")
+	return parts[0], nil
 }
 
 func ListenString() string {
