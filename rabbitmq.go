99cfac284eea0d7057b9fa8f674fdd81a27ee0b8
Andy Lo-A-Foe <andy.loafoe@aemian.com>
2016-10-07 15:50:20 +0200

    Better reconnect logic

--- rabbitmq.go
+++ rabbitmq.go
@@ -6,6 +6,7 @@ import (
 	cfenv "github.com/cloudfoundry-community/go-cfenv"
 	"github.com/mitchellh/mapstructure"
 	"github.com/streadway/amqp"
+	"time"
 )
 
 func RabbitMQAdminURI(serviceName string) (string, error) {
@@ -52,10 +53,16 @@ func RabbitMQAdminURI(serviceName string) (string, error) {
 }
 
 type Consumer struct {
-	conn    *amqp.Connection
-	channel *amqp.Channel
-	tag     string
-	done    chan error
+	conn         *amqp.Connection
+	channel      *amqp.Channel
+	handlerFunc  ConsumerHandlerFunc
+	done         chan error
+	consumerTag  string // Name that consumer identifies itself to the server with
+	uri          string // uri of the rabbitmq server
+	exchange     string // exchange that we will bind to
+	exchangeType string // topic, direct, etc...
+	bindingKey   string // routing key that we are using
+	queueName    string // queue name
 }
 
 type Producer struct {
@@ -147,128 +154,197 @@ func NewConsumer(config ConsumerConfig) (*Consumer, error) {
 	}
 
 	c := &Consumer{
-		conn:    nil,
-		channel: nil,
-		tag:     config.CTag,
-		done:    make(chan error),
+		conn:         nil,
+		channel:      nil,
+		uri:          connectString,
+		handlerFunc:  config.HandlerFunc,
+		consumerTag:  config.CTag,
+		exchange:     config.Exchange,
+		exchangeType: config.ExchangeType,
+		bindingKey:   config.RoutingKey,
+		queueName:    config.QueueName,
+		done:         make(chan error),
+	}
+	return c, err
+}
+
+func (c *Consumer) Start() error {
+	if err := c.Connect(); err != nil {
+		return err
 	}
 
-	log.Debug(nil, "dialing %q", connectString)
-	c.conn, err = amqp.Dial(connectString)
+	deliveries, err := c.AnnounceQueue(c.queueName, c.bindingKey)
 	if err != nil {
-		return nil, fmt.Errorf("Dial: %s", err)
+		return err
+	}
+	go c.Handle(deliveries, c.handlerFunc, 1, c.queueName, c.bindingKey)
+	return nil
+}
+
+// ReConnect is called in places where NotifyClose() channel is called
+// wait 30 seconds before trying to reconnect. Any shorter amount of time
+// will  likely destroy the error log while waiting for servers to come
+// back online. This requires two parameters which is just to satisfy
+// the AccounceQueue call and allows greater flexability
+func (c *Consumer) ReConnect(queueName, bindingKey string) (<-chan amqp.Delivery, error) {
+	time.Sleep(30 * time.Second)
+
+	if err := c.Connect(); err != nil {
+		log.Info(nil, "Could not connect in reconnect call: %v", err.Error())
+	}
+
+	deliveries, err := c.AnnounceQueue(queueName, bindingKey)
+	if err != nil {
+		return deliveries, errors.New("Couldn't connect")
+	}
+
+	return deliveries, nil
+}
+
+// Connect to RabbitMQ server
+func (c *Consumer) Connect() error {
+
+	var err error
+
+	log.Info(nil, "dialing %q", c.uri)
+	c.conn, err = amqp.Dial(c.uri)
+	if err != nil {
+		return fmt.Errorf("Dial: %s", err)
 	}
 
 	go func() {
+		// Waits here for the channel to be closed
 		log.Info(nil, "closing: %s", <-c.conn.NotifyClose(make(chan *amqp.Error)))
+		// Let Handle know it's not time to reconnect
+		c.done <- errors.New("Channel Closed")
 	}()
 
-	log.Debug(nil, "got Connection, getting Channel")
+	log.Info(nil, "got Connection, getting Channel")
 	c.channel, err = c.conn.Channel()
 	if err != nil {
-		c.conn.Close()
-		return nil, fmt.Errorf("Channel: %s", err)
+		return fmt.Errorf("Channel: %s", err)
 	}
 
-	err = c.channel.Qos(2, 0, false)
-	if err != nil {
-		log.Error(nil, "error setting Qos: %s", err)
-	}
-
-	log.Info(nil, "got Channel, declaring Exchange (%q)", config.Exchange)
+	log.Info(nil, "got Channel, declaring Exchange (%q)", c.exchange)
 	if err = c.channel.ExchangeDeclare(
-		config.Exchange,     // name of the exchange
-		config.ExchangeType, // type
-		true,                // durable
-		false,               // delete when complete
-		false,               // internal
-		false,               // noWait
-		nil,                 // arguments
+		c.exchange,     // name of the exchange
+		c.exchangeType, // type
+		true,           // durable
+		false,          // delete when complete
+		false,          // internal
+		false,          // noWait
+		nil,            // arguments
 	); err != nil {
-		c.conn.Close()
-		return nil, fmt.Errorf("Exchange Declare: %s", err)
+		return fmt.Errorf("Exchange Declare: %s", err)
 	}
 
-	log.Info(nil, "declared Exchange, declaring Queue %q", config.QueueName)
+	return nil
+}
+
+// AnnounceQueue sets the queue that will be listened to for this
+// connection...
+func (c *Consumer) AnnounceQueue(queueName, bindingKey string) (<-chan amqp.Delivery, error) {
+	log.Info(nil, "declared Exchange, declaring Queue %q", queueName)
 	queue, err := c.channel.QueueDeclare(
-		config.QueueName, // name of the queue
-		true,             // durable
-		false,            // delete when usused
-		false,            // exclusive
-		false,            // noWait
-		nil,              // arguments
+		queueName, // name of the queue
+		true,      // durable
+		false,     // delete when usused
+		false,     // exclusive
+		false,     // noWait
+		nil,       // arguments
 	)
+
 	if err != nil {
 		return nil, fmt.Errorf("Queue Declare: %s", err)
 	}
 
 	log.Info(nil, "declared Queue (%q %d messages, %d consumers), binding to Exchange (key %q)",
-		queue.Name, queue.Messages, queue.Consumers, config.RoutingKey)
+		queue.Name, queue.Messages, queue.Consumers, bindingKey)
+
+	// Qos determines the amount of messages that the queue will pass to you before
+	// it waits for you to ack them. This will slow down queue consumption but
+	// give you more certainty that all messages are being processed. As load increases
+	// I would reccomend upping the about of Threads and Processors the go process
+	// uses before changing this although you will eventually need to reach some
+	// balance between threads, procs, and Qos.
+	err = c.channel.Qos(50, 0, false)
+	if err != nil {
+		return nil, fmt.Errorf("Error setting qos: %s", err)
+	}
 
 	if err = c.channel.QueueBind(
-		queue.Name,        // name of the queue
-		config.RoutingKey, // bindingKey
-		config.Exchange,   // sourceExchange
-		false,             // noWait
-		nil,               // arguments
+		queue.Name, // name of the queue
+		bindingKey, // bindingKey
+		c.exchange, // sourceExchange
+		false,      // noWait
+		nil,        // arguments
 	); err != nil {
-		c.conn.Close()
 		return nil, fmt.Errorf("Queue Bind: %s", err)
 	}
 
-	log.Info(nil, "Queue bound to Exchange, starting Consume (consumer tag %q)", c.tag)
+	log.Info(nil, "Queue bound to Exchange, starting Consume (consumer tag %q)", c.consumerTag)
 	deliveries, err := c.channel.Consume(
-		queue.Name, // name
-		c.tag,      // consumerTag,
-		false,      // noAck
-		false,      // exclusive
-		false,      // noLocal
-		false,      // noWait
-		nil,        // arguments
+		queue.Name,    // name
+		c.consumerTag, // consumerTag,
+		false,         // noAck
+		false,         // exclusive
+		false,         // noLocal
+		false,         // noWait
+		nil,           // arguments
 	)
 	if err != nil {
-		c.conn.Close()
 		return nil, fmt.Errorf("Queue Consume: %s", err)
 	}
 
-	go handle(config.HandlerFunc, deliveries, c.done)
-
-	return c, nil
+	return deliveries, nil
 }
 
-func (c *Consumer) Shutdown() error {
-	// will close() the deliveries channel
-	if err := c.channel.Cancel(c.tag, true); err != nil {
-		return fmt.Errorf("Consumer cancel failed: %s", err)
+// Handle has all the logic to make sure your program keeps running
+// d should be a delievey channel as created when you call AnnounceQueue
+// fn should be a function that handles the processing of deliveries
+// this should be the last thing called in main as code under it will
+// become unreachable unless put int a goroutine. The q and rk params
+// are redundent but allow you to have multiple queue listeners in main
+// without them you would be tied into only using one queue per connection
+func (c *Consumer) Handle(
+	d <-chan amqp.Delivery,
+	fn ConsumerHandlerFunc,
+	threads int,
+	queue string,
+	routingKey string) {
+
+	var err error
+
+	for {
+		for i := 0; i < threads; i++ {
+			go fn(d)
+		}
+
+		// Go into reconnect loop when
+		// c.done is passed non nil values
+		if <-c.done != nil {
+			d, err = c.ReConnect(queue, routingKey)
+			if err != nil {
+				// Very likely chance of failing
+				// should not cause worker to terminate
+				log.Error(nil, "Reconnecting Error: %s", err)
+			}
+		}
+		log.Info(nil, "Reconnected... possibly")
 	}
-
-	if err := c.conn.Close(); err != nil {
-		return fmt.Errorf("AMQP connection close error: %s", err)
-	}
-
-	defer log.Info(nil, "AMQP shutdown OK")
-
-	// wait for handle() to exit
-	return <-c.done
 }
 
-type ConsumerHandlerFunc func(delivery amqp.Delivery) error
+type ConsumerHandlerFunc func(deliveries <-chan amqp.Delivery) error
 
-func DummyConsumerHandler(d amqp.Delivery) error {
-	log.Debug(nil,
-		"got %dB delivery: [%v] %q",
-		len(d.Body),
-		d.DeliveryTag,
-		d.Body,
-	)
-	d.Ack(false)
-	return nil
-}
-
-func handle(handler ConsumerHandlerFunc, deliveries <-chan amqp.Delivery, done chan error) {
+func DummyConsumerHandler(deliveries <-chan amqp.Delivery) error {
 	for d := range deliveries {
-		handler(d)
+		log.Debug(nil,
+			"got %dB delivery: [%v] %q",
+			len(d.Body),
+			d.DeliveryTag,
+			d.Body,
+		)
+		d.Ack(false)
 	}
-	log.Info(nil, "handle: deliveries channel closed")
-	done <- nil
+	return nil
 }
