31385baa94f9b789d48946e22bde04108e3dde54
Andy Lo-A-Foe <andy.loafoe@aemian.com>
2017-06-28 23:40:12 +0200

    Force local emalation on CF_LOCAL=true

--- current.go
+++ current.go
@@ -7,11 +7,10 @@ import (
 
 func Current() (appEnv *cfenv.App, err error) {
 	appEnv, err = cfenv.Current()
-	if err != nil {
+	if os.Getenv("CF_LOCAL") == "true" || err != nil {
 		os.Setenv("VCAP_APPLICATION", localVcapApplication())
 		os.Setenv("MEMORY_LIMIT", localMemoryLimit())
 		os.Setenv("VCAP_SERVICES", localVcapServices())
-		os.Setenv("CF_LOCAL", "true")
 		appEnv, err = cfenv.Current()
 	}
 	return appEnv, err
